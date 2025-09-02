e9b7ce219707932ce1a166d7a447c6340d46c47d
konstantin.dubanevich <k.dubanevich@belitsoft.com>
2025-09-02 22:04:00 +0300

    Add task5

--- src/Task5.java
+++ src/Task5.java
@@ -1,5 +1,24 @@
 public class Task5 {
     public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int value = 2;
+        int index = 0;
+        int[] newArray = new int[array.length - 1];
 
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                index = i;
+                break;
+            }
+        }
+
+        for (int i = 0, j = 0; i < array.length && j < newArray.length; i++, j++) {
+            if (i == index) {
+                i++;
+            }
+
+            newArray[j] = array[i];
+            System.out.println(newArray[j]);
+        }
     }
 }
